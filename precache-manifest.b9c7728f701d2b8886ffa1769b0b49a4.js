64e79da2d34c347a4ff7f15cd7500be0f13420b3
Shubham Ginde <shubhginde@gmail.com>
2020-09-25 11:35:07 +0530

    Updates

---
+++ precache-manifest.b9c7728f701d2b8886ffa1769b0b49a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429ce86550f0bac17aefc7b93dd4982c",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "ac2c407746a55705f8e1",
+    "url": "/react-todo-app/static/js/2.9ce88a01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.9ce88a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73788eb5d4d891e8a4ca",
+    "url": "/react-todo-app/static/js/main.587f026c.chunk.js"
+  },
+  {
+    "revision": "57ec107919da485ffd68",
+    "url": "/react-todo-app/static/js/runtime-main.d5b6ac6a.js"
+  },
+  {
+    "revision": "bf2628e43f68f292fa97d1a51a9b0a73",
+    "url": "/react-todo-app/static/media/close.bf2628e4.png"
+  }
+]);
